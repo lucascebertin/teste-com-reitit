3744a81fee78e3ac8751a4f44c4f8ccf47a1bca4
Lucas <lucas.ce.bertin@gmail.com>
2021-07-27 19:15:27 -0300

    fix: corrige indentação de argumentos

--- src/teste_com_reitit/database.clj
+++ src/teste_com_reitit/database.clj
@@ -16,12 +16,14 @@
   (map sql-todo->todo-entity sql-todos))
 
 (s/defn insert-todo! :- schemas/TodoEntity
-  [{{dataset :dataset} :database} todo :- schemas/TodoIn]
+  [{{dataset :dataset} :database} 
+   todo :- schemas/TodoIn]
   (let [new-register (sql/insert! (dataset) :todo_list todo)]
     (sql-todo->todo-entity new-register)))
 
 (s/defn get-todo-by-id! :- schemas/TodoEntity
-  [{{dataset :dataset} :database} id :- s/Int]
+  [{{dataset :dataset} :database} 
+   id :- s/Int]
   (let [register (sql/get-by-id (dataset) :todo_list id)]
     (sql-todo->todo-entity register)))
 
@@ -31,11 +33,13 @@
     (sql-todos->todo-entities registers)))
 
 (s/defn update-todo! :- schemas/TodoEntity
-  [{{dataset :dataset} :database} id :- s/Int todo :- schemas/TodoIn]
+  [{{dataset :dataset} :database} 
+   id :- s/Int todo :- schemas/TodoIn]
   (sql/update! (dataset) :todo_list todo {:id id})
   (merge todo {:id id}))
 
 (s/defn delete-todo!
-  [{{dataset :dataset} :database} id :- s/Int]
+  [{{dataset :dataset} :database} 
+   id :- s/Int]
   (sql/delete! (dataset) :todo_list {:id id}))
   
