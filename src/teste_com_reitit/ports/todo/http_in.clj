04b15163fcf73c52a8de5f2490fa7c0a18011299
Lucas <lucas.ce.bertin@gmail.com>
2021-07-27 19:21:01 -0300

    fix: corrige indentação de argumentos

--- src/teste_com_reitit/ports/todo/http_in.clj
+++ src/teste_com_reitit/ports/todo/http_in.clj
@@ -11,24 +11,28 @@
       {:status 200 :body todos})))
 
 (s/defn add-todo :- schemas/TodoOut
-  [{{database :database} :components {body :body} :parameters}]
+  [{{database :database} :components 
+    {body :body} :parameters}]
   (let [todo (db/insert-todo! database body)]
     {:status 201 :body todo}))
 
 (s/defn get-todo-by-id :- schemas/TodoOut
-  [{{database :database} :components {{id :id} :query} :parameters}]
+  [{{database :database} :components 
+    {{id :id} :query} :parameters}]
   (let [todo (db/get-todo-by-id! database id)]
     (when todo
       {:status 200 :body todo}
       {:status 404 :body "Todo not found"})))
 
 (s/defn update-todo-by-id :- schemas/TodoOut
-  [{{database :database} :components {{id :id} :query body :body} :parameters}]
+  [{{database :database} :components 
+    {{id :id} :query body :body} :parameters}]
   (let [todo (db/update-todo! database id body)]
     {:status 200 :body todo}))
 
 (s/defn delete-todo-by-id :- schemas/TodoOut
-  [{{database :database} :components {{id :id} :query} :parameters}]
+  [{{database :database} :components 
+    {{id :id} :query} :parameters}]
   (db/delete-todo! database id)
   {:status 200})
 
